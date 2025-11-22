92bef1f5206a5e917e94e17f970d54046cd0e1c4
AlyssaHug <AlyssaHug@shaw.ca>
2025-11-21 22:28:26 -0800

    add and position  images + header to hayley page

--- src/app/Band/Hayley/page.js
+++ src/app/Band/Hayley/page.js
@@ -5,9 +5,22 @@ export default function Page() {
     return (
         <div className={styles.content}>
             <div className={styles.left}>
-                <h1>Singer</h1>
-                <h2>Hayley Williams</h2>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>Singer</h1>
+                    <h2 className={styles.caption}>Hayley Williams</h2>
+                </div>
+                <img
+                    className={styles.close}
+                    src='/Band/Hayley/Close-up.png'
+                    alt='Hayley Williams'
+                />
+                <img
+                    className={styles.mic}
+                    src='/Band/Hayley/Mic.png'
+                    alt='microphone graphic'
+                />
             </div>
+            <p className={styles.spacer}>Hayley</p>
             <div className={styles.right}></div>
         </div>
     );
