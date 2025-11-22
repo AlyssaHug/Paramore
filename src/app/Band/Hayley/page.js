f2c59341b41a5b36a9c726b9c31c020fb0b2ba52
AlyssaHug <AlyssaHug@shaw.ca>
2025-11-21 22:03:35 -0800

    start hayley page and adjusted styles for better compatability

--- src/app/Band/Hayley/page.js
+++ src/app/Band/Hayley/page.js
@@ -3,8 +3,12 @@ import styles from "../Hayley/hayley.module.css";
 
 export default function Page() {
     return (
-        <div>
-            <h1>HAYLEY WILLIAMS OMG</h1>
+        <div className={styles.content}>
+            <div className={styles.left}>
+                <h1>Singer</h1>
+                <h2>Hayley Williams</h2>
+            </div>
+            <div className={styles.right}></div>
         </div>
     );
 }
--- src/app/Band/page.js
+++ src/app/Band/page.js
@@ -4,45 +4,92 @@ import Link from "next/link";
 
 function Page() {
     return (
-        <>
-            <div className={styles.header}>
-                <h1 className={styles.title}>Meet The Band</h1>
-                <p className={styles.caption}>The faces behind the music</p>
+        <div className={styles.content}>
+            <div className={styles.body}>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>Meet The Band</h1>
+                    <p className={styles.caption}>The faces behind the music</p>
+                </div>
+                <div className='band'>
+                    <Link
+                        className={styles.taylor}
+                        href='/Band/Taylor'
+                        id={styles.left}>
+                        <img
+                            className={styles.bandTaylor}
+                            src='/Band/Home/Taylor.png'
+                            alt='Taylor York'
+                        />
+                        <img
+                            className={styles.bandTaylor}
+                            src='/Band/Home/Taylor_Ver01.png'
+                            alt='Taylor York'
+                        />
+                        <img
+                            className={styles.bandTaylor}
+                            src='/Band/Home/Taylor_Ver02.png'
+                            alt='Taylor York'
+                        />
+                        <img
+                            className={styles.bandTaylor}
+                            src='/Band/Home/Taylor_Ver03.png'
+                            alt='Taylor York'
+                        />
+                    </Link>
+                    <Link
+                        className={styles.hayley}
+                        id={styles.center}
+                        href='/Band/Hayley'>
+                        <img
+                            className={styles.bandHayley}
+                            src='/Band/Home/Hayley.png'
+                            alt='Hayley Williams'
+                        />
+                        <img
+                            className={styles.bandHayley}
+                            src='/Band/Home/Hayley_Ver01.png'
+                            alt='Hayley Williams'
+                        />
+                        <img
+                            className={styles.bandHayley}
+                            src='/Band/Home/Hayley_Ver02.png'
+                            alt='Hayley Williams'
+                        />
+                        <img
+                            className={styles.bandHayley}
+                            src='/Band/Home/Hayley_Ver03.png'
+                            alt='Hayley Williams'
+                        />
+                    </Link>
+                    <Link
+                        className={styles.zac}
+                        href='/Band/Zac'
+                        id={styles.right}>
+                        <img
+                            className={styles.bandZac}
+                            src='/Band/Home/Zac.png'
+                            alt='Zac Farro'
+                        />
+                        <img
+                            className={styles.bandZac}
+                            src='/Band/Home/Zac_Ver01.png'
+                            alt='Zac Farro'
+                        />
+                        <img
+                            className={styles.bandZac}
+                            src='/Band/Home/Zac_Ver02.png'
+                            alt='Zac Farro'
+                        />
+                        <img
+                            className={styles.bandZac}
+                            src='/Band/Home/Zac_Ver03.png'
+                            alt='Zac Farro'
+                        />
+                    </Link>
+                    <p className={styles.spacer}>Paramore</p>
+                </div>
             </div>
-            <div className='band'>
-                <Link
-                    className={styles.taylor}
-                    href='/Band/Taylor'
-                    id={styles.left}>
-                    <img
-                        className={styles.bandTaylor}
-                        src='/Band/Home/Taylor.png'
-                        alt='Taylor York'
-                    />
-                </Link>
-                <Link
-                    className={styles.hayley}
-                    id={styles.center}
-                    href='/Band/Hayley'>
-                    <img
-                        className={styles.bandHayley}
-                        src='/Band/Home/Hayley.png'
-                        alt='Hayley Williams'
-                    />
-                </Link>
-                <Link
-                    className={styles.zac}
-                    href='/Band/Zac'
-                    id={styles.right}>
-                    <img
-                        className={styles.bandZac}
-                        src='/Band/Home/Zac.png'
-                        alt='Zac Farro'
-                    />
-                </Link>
-                <p className={styles.spacer}>Paramore</p>
-            </div>
-        </>
+        </div>
     );
 }
 
