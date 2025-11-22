7d82a770ad30181092bb89be6c75cf96e8f59815
AlyssaHug <AlyssaHug@shaw.ca>
2025-11-21 20:48:25 -0800

    tested image linking to page + add person pages

---
+++ src/app/Band/Hayley/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "../Hayley/hayley.module.css";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>HAYLEY WILLIAMS OMG</h1>
+        </div>
+    );
+}
--- src/app/Band/page.js
+++ src/app/Band/page.js
@@ -1,29 +1,45 @@
 import styles from "../Band/band.module.css";
 import classNames from "classnames";
+import Link from "next/link";
 
 function Page() {
     return (
         <>
             <div className={styles.header}>
-                <h1>Meet The Band</h1>
+                <h1 className={styles.title}>Meet The Band</h1>
                 <p className={styles.caption}>The faces behind the music</p>
             </div>
             <div className='band'>
-                <img
-                    className={styles.bandTaylor}
-                    src='/Band/Home/Taylor.png'
-                    alt='Taylor York'
-                />
-                <img
-                    className={styles.bandHayley}
-                    src='/Band/Home/Hayley.png'
-                    alt='Hayley Williams'
-                />
-                <img
-                    className={styles.bandZac}
-                    src='/Band/Home/Zac.png'
-                    alt='Zac Farro'
-                />
+                <Link
+                    className={styles.taylor}
+                    href='/Band/Taylor'
+                    id={styles.left}>
+                    <img
+                        className={styles.bandTaylor}
+                        src='/Band/Home/Taylor.png'
+                        alt='Taylor York'
+                    />
+                </Link>
+                <Link
+                    className={styles.hayley}
+                    id={styles.center}
+                    href='/Band/Hayley'>
+                    <img
+                        className={styles.bandHayley}
+                        src='/Band/Home/Hayley.png'
+                        alt='Hayley Williams'
+                    />
+                </Link>
+                <Link
+                    className={styles.zac}
+                    href='/Band/Zac'
+                    id={styles.right}>
+                    <img
+                        className={styles.bandZac}
+                        src='/Band/Home/Zac.png'
+                        alt='Zac Farro'
+                    />
+                </Link>
                 <p className={styles.spacer}>Paramore</p>
             </div>
         </>
