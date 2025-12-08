19e94208b043ea1785f8815dbfee533822f25cc7
AlyssaHug <AlyssaHug@shaw.ca>
2025-12-07 18:47:32 -0800

    change Hayley band photo + build NExt component + make files for music palyer

--- src/app/Band/Hayley/page.js
+++ src/app/Band/Hayley/page.js
@@ -1,33 +1,37 @@
 import Link from "next/link";
 import styles from "../Hayley/hayley.module.css";
+import Next from "@/app/components/NextBtn/NextBtn";
 
 export default function Page() {
     return (
-        <div className={styles.content}>
-            <div className={styles.left}>
-                <div className={styles.header}>
-                    <h1 className={styles.title}>Singer</h1>
-                    <h2 className={styles.caption}>Hayley Williams</h2>
+        <div>
+            <Next value='Their History' />
+            <div className={styles.content}>
+                <div className={styles.left}>
+                    <div className={styles.header}>
+                        <h1 className={styles.title}>Singer</h1>
+                        <h2 className={styles.caption}>Hayley Williams</h2>
+                    </div>
+                    <img
+                        className={styles.close}
+                        src='/Band/Hayley/Close-up.png'
+                        alt='Hayley Williams'
+                    />
+                    <img
+                        className={styles.mic}
+                        src='/Band/Hayley/Mic.png'
+                        alt='microphone graphic'
+                    />
+                    <p className={styles.spacer}>Hayley</p>
                 </div>
-                <img
-                    className={styles.close}
-                    src='/Band/Hayley/Close-up.png'
-                    alt='Hayley Williams'
-                />
-                <img
-                    className={styles.mic}
-                    src='/Band/Hayley/Mic.png'
-                    alt='microphone graphic'
-                />
-                <p className={styles.spacer}>Hayley</p>
-            </div>
-            <div className={styles.right}>
-                <img
-                    className={styles.perform}
-                    src='/Band/Hayley/Perform.png'
-                    alt='Hayley singing on stage opening for Taylor Swift'
-                />
-                <p className={styles.desc}>lorem ipsum</p>
+                <div className={styles.right}>
+                    <img
+                        className={styles.perform}
+                        src='/Band/Hayley/Perform.png'
+                        alt='Hayley singing on stage opening for Taylor Swift'
+                    />
+                    <p className={styles.desc}>lorem ipsum</p>
+                </div>
             </div>
         </div>
     );
---
+++ src/app/components/NextBtn/NextBtn.js
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import styles from "../NextBtn/NextBtn.module.css";
+
+export default function Next({ href, value, target = "_self" }) {
+    function handleClick() {
+        if (href) {
+            window.open(href, target);
+            return;
+        }
+        if (onClick) {
+            onClick();
+        }
+    }
+    return (
+        <div className={styles.btnRow}>
+            <button
+                onClick={handleClick}
+                className={styles.nextBtn}>
+                <p>Next Up: {value}</p>
+            </button>
+        </div>
+    );
+}
