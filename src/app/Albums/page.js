c1a92ee14a5f29ce89f676f5fee8a8eddc95b20e
AlyssaHug <AlyssaHug@shaw.ca>
2025-12-09 16:45:32 -0800

    Build album homepage + fill future content + fix styling in places idk anymore man

--- src/app/Albums/page.js
+++ src/app/Albums/page.js
@@ -1,28 +1,56 @@
-import React from "react";
+// app/albums/page.jsx
 import Link from "next/link";
+import AlbumsPile from "../components/AlbumPile/AlbumsPile";
+import styles from "./main.module.css";
+import NextBtn from "../components/NextBtn/NextBtn";
+import Header from "../components/Header/Header";
 
-export default function AlbumPage() {
+export default function AlbumsPage() {
+    const paramoreAlbums = [
+        {
+            id: "AllWeKnowIsFalling",
+            title: "All We Know Is Falling",
+            year: 2005,
+            cover: "/Home/AllWeKnow.png",
+        },
+        { id: "Riot", title: "Riot!", year: 2007, cover: "/Home/Riot.png" },
+        {
+            id: "BrandNewEyes",
+            title: "Brand New Eyes",
+            year: 2009,
+            cover: "/Home/BrandNew.png",
+        },
+        {
+            id: "Paramore",
+            title: "Paramore",
+            year: 2013,
+            cover: "/Home/Paramore.png",
+        },
+        {
+            id: "AfterLaughter",
+            title: "After Laughter",
+            year: 2017,
+            cover: "/Home/AfterLaughter.png",
+        },
+        {
+            id: "ThisIsWhy",
+            title: "This Is Why",
+            year: 2023,
+            cover: "/Home/ThisIsWhy.png",
+        },
+    ];
+
     return (
         <div>
-            <main>
-            <Link href="/Albums/AllWeKnowIsFalling">
-    <button>Go to All We Know Is Falling</button>
-  </Link>
-                 <Link href='/Albums/Riot'>
-                    <button>Go to Riot</button>
-                </Link>
-                <Link href='/Albums/BrandNewEyes'>
-                    <button>Go to Brand New Eyes</button>
-                </Link>
-                 <Link href='/Albums/Paramore'>
-                    <button>Go to Paramore</button>
-                </Link>
-                 <Link href='/Albums/AfterLaughter'>
-                    <button>Go to After Laughter</button>
-                </Link>
-                 <Link href='/Albums/ThisisWhy'>
-                    <button>Go to This is Why</button>
-                </Link>
+            <Header value='The Albums' />
+            <NextBtn
+                backLabel='Previous: History'
+                backHref='/History'
+                nextLabel='Next up: Future'
+                nextHref='/Future'
+            />
+            <main className={styles.main}>
+                <AlbumsPile albums={paramoreAlbums} />
             </main>
         </div>
     );
--- src/app/Future/page.js
+++ src/app/Future/page.js
@@ -1,14 +1,16 @@
 import styles from "@/app/Future/future.module.css";
 import NextBtn from "../components/NextBtn/NextBtn";
 import ScrollText from "../components/ScrollText/ScrollText";
+import Header from "../components/Header/Header";
 
 export default function Page() {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.content}>
+            <Header value='The Present' />
+            <div className={styles.head}>
                 <NextBtn
                     nextLabel='Next Up: Thank you'
-                    nextHref='/End'
+                    nextHref='/ThankYou'
                     backLabel='Previous: Albums'
                     backHref='/Albums'
                 />
@@ -17,10 +19,20 @@ export default function Page() {
                 </div>
                 <div className={styles.column}>
                     <div className={styles.header}>
-                        <h1 className={styles.title}>The Present</h1>
-                        <p className={styles.caption}>Where are they now?</p>
+                        <p className={styles.title}>Where are they now?</p>
                     </div>
-                    <p className={styles.bodyText}>bla bla blahhhhhh</p>
+                    <p className={styles.bodyText}>
+                        Their most recent release was their 20th anniversary
+                        edition of their debut album, “All we know is falling”
+                        in July 2025. As of September 2025, the band has
+                        announced their on a break for the moment, not broken
+                        up. With their 20-year label contract having expired,
+                        the members are exploring what they are as a band and
+                        who they are as people outside of their contract. With
+                        the breaks between albums being long previously, this is
+                        normal for the band, so they can also live their lives
+                        as individuals.
+                    </p>
                 </div>
 
                 <img
@@ -29,6 +41,73 @@ export default function Page() {
                     alt='Picture of Hayley, Taylor, and Zac on the couch in an interview'
                 />
                 <div className={styles.pageColumn}></div>
+            </div>
+            <div className={styles.divider}>"paramour" 'Secret Lover'</div>
+            <div className={styles.hayley}>
+                <img
+                    className={styles.hayleyImg}
+                    src='/Future/hayley.webp'
+                    alt='Hayley in a bridal dress with food and drinks around her from her solo tour poster'
+                />
+                <div className={styles.hayleyText}>
+                    <h2 className={styles.Subhead}>
+                        Hayley Williams - Third Solo Album
+                    </h2>
+                    <p className={styles.subText}>
+                        In August 2025, Hayley released a surprise collection of
+                        17 songs, which were later packaged as the album “Ego
+                        Death at a Bachelorette Party” in November with 3 other
+                        tracks. She has announced her first solo shows for 2026,
+                        something she tried doing for her prior solo work in
+                        2020/2021 but couldn’t due to the pandemic. She is
+                        currently working through finding herself outside the
+                        identity of Paramore with her solo venture.
+                    </p>
+                </div>
+            </div>
+            <ScrollText text='Ego Death at a bachelorette party' />
+            <div className={styles.zac}>
+                <div className={styles.zacText}>
+                    <h2 className={styles.Subhead}>
+                        Zac Farro - Debut Solo Album
+                    </h2>
+                    <p className={styles.subText}>
+                        In July 2025, Zac released his debut solo album under
+                        his own name. His album “Operator”, explores themes like
+                        nostalgia, love, grief, and communication, different
+                        from his prior work in Paramore. This music allows him
+                        to also explore who he is outside of the band, showing a
+                        different side to himself.
+                    </p>
+                </div>
+                <img
+                    className={styles.zacImg}
+                    src='/Future/zac.jpg'
+                    alt='/'
+                />
+            </div>
+            <ScrollText text='Operator • Operator' />
+            <div className={styles.taylor}>
+                <img
+                    className={styles.taylorImg}
+                    src='/Future/taylor.jpg'
+                    alt='Taylor York holding a guitar posing for a photo'
+                />
+                <div className={styles.taylorText}>
+                    <h2 className={styles.Subhead}>
+                        Taylor York - Low Profile
+                    </h2>
+                    <p className={styles.subText}>
+                        With the band's wiped social media presence earlier in
+                        the year, there was uncertainty with its members. But in
+                        September 2025, Taylor re-launched his instagram page
+                        listing himself as the band’s guitarist, confirming he
+                        remains part of the band. Other than his instagram
+                        reboot, his public activity has been minimal, with no
+                        links to any side-projects or music this year. The
+                        band's break has allowed him to step back for awhile.
+                    </p>
+                </div>
             </div>
         </div>
     );
